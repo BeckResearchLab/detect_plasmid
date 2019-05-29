df2cb231aaf49a1aeb987c68476d98b150545307
Dave Beck <dacb@uw.edu>
2019-05-29 07:27:05 -0700

    working architecture, need to join to FNA

--- plasmid_cds_extractor.py
+++ plasmid_cds_extractor.py
@@ -23,15 +23,15 @@ def plasmid_cds_extractor(plasmids_fna_path, plasmids_gbff_path, output_file):
 
     start_time = datetime.now()
 
-    print(f'reading FASTA file {plasmids_fna_path}')
-    print(f'reading GBFF file {plasmids_gbff_path}')
-
+    print(f'writing records to {output_file}')
     f = open(output_file, 'w')
     f.write('gff_file\tid\tkingdom\tphylum\tclass\torder\tfamily\tgenus\tproduct_id\tsequence\tis_plasmid\n')
     f.flush()
 
+    print(f'reading FASTA file {plasmids_fna_path}')
     fna_dict = SeqIO.to_dict(SeqIO.parse(plasmids_fna_path, 'fasta'))
 
+    print(f'reading GBFF file {plasmids_gbff_path}')
     try:
         for seq_record in SeqIO.parse(plasmids_gbff_path, 'genbank'):
             taxonomy = seq_record.annotations['taxonomy']
@@ -39,9 +39,9 @@ def plasmid_cds_extractor(plasmids_fna_path, plasmids_gbff_path, output_file):
                 if feature.type == 'CDS':
                     if 'contig' in seq_record.annotations:
                         seq = 'NN'
-                        f.write(f"{filepath}\t{seq_record.id}\t{taxonomy[0] if len(taxonomy) > 0 else np.nan}\t{taxonomy[1] if len(taxonomy) > 1 else np.nan}\t{taxonomy[2] if len(taxonomy) > 2 else np.nan}\t{taxonomy[3] if len(taxonomy) > 3 else np.nan}\t{taxonomy[4] if len(taxonomy) > 4 else np.nan}\t{taxonomy[5] if len(taxonomy) > 5 else np.nan}\t{feature.qualifiers['protein_id'][0] if 'protein_id' in feature.qualifiers else np.nan}\t{seq}\t1\n")
+                        f.write(f"{plasmids_gbff_path}\t{seq_record.id}\t{taxonomy[0] if len(taxonomy) > 0 else np.nan}\t{taxonomy[1] if len(taxonomy) > 1 else np.nan}\t{taxonomy[2] if len(taxonomy) > 2 else np.nan}\t{taxonomy[3] if len(taxonomy) > 3 else np.nan}\t{taxonomy[4] if len(taxonomy) > 4 else np.nan}\t{taxonomy[5] if len(taxonomy) > 5 else np.nan}\t{feature.qualifiers['protein_id'][0] if 'protein_id' in feature.qualifiers else np.nan}\t{seq}\t1\n")
                     else:
-                        output.write(f"{filepath}\t{seq_record.id}\t{taxonomy[0] if len(taxonomy) > 0 else np.nan}\t{taxonomy[1] if len(taxonomy) > 1 else np.nan}\t{taxonomy[2] if len(taxonomy) > 2 else np.nan}\t{taxonomy[3] if len(taxonomy) > 3 else np.nan}\t{taxonomy[4] if len(taxonomy) > 4 else np.nan}\t{taxonomy[5] if len(taxonomy) > 5 else np.nan}\t{feature.qualifiers['protein_id'][0] if 'protein_id' in feature.qualifiers else np.nan}\t{feature.location.extract(seq_record).seq}\t1\n")
+                        f.write(f"{plasmids_gbff_path}\t{seq_record.id}\t{taxonomy[0] if len(taxonomy) > 0 else np.nan}\t{taxonomy[1] if len(taxonomy) > 1 else np.nan}\t{taxonomy[2] if len(taxonomy) > 2 else np.nan}\t{taxonomy[3] if len(taxonomy) > 3 else np.nan}\t{taxonomy[4] if len(taxonomy) > 4 else np.nan}\t{taxonomy[5] if len(taxonomy) > 5 else np.nan}\t{feature.qualifiers['protein_id'][0] if 'protein_id' in feature.qualifiers else np.nan}\t{feature.location.extract(seq_record).seq}\t1\n")
     except AttributeError:
         print(f'parsing of file {filepath} failed')
 
