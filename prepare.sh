5afa1992ab8e1b7321299cae56dadc2db1efa261
Dave Beck <dacb@uw.edu>
2019-05-30 13:52:31 -0700

    fixed up line ending issues

--- prepare.sh
+++ prepare.sh
@@ -3,33 +3,27 @@
 REFSEQ_PATH=/work/data/refseq
 THREADS=24
 MIN_SEQ_LEN=300
-#CLASS_SAMPLES=100000
-
-./plasmid_cds_extractor.py -f plasmids/ncbi_plasmid.fna -g plasmids/ncbi_plasmid.gbff -o plasmid_cds.tsv
-./refseq_cds_integrate.py -r /work/dacb/detect_hgt/300bp/refseq_cds_filtered.tsv -p plasmid_cds.tsv -o all_cds.tsv
-./cds_balance
-./all_cds_filter
-
-exit
+CLASS_SAMPLES=100000
 
 
 
-if [ ! -e refseq_cds.tsv ]; then
-	./refseq_cds_extractor.py --refseq_path $REFSEQ_PATH \
-			--output_file refseq_cds.tsv --threads $THREADS --include_all True
+if [ ! -e plasmid_cds.tsv ]; then
+	./plasmid_cds_extractor.py --plasmids_fna plasmids/ncbi_plasmid.fna --plasmids_gbff plasmids/ncbi_plasmid.gbff \
+			--output_file plasmid_cds.tsv
 fi
 
-if [ ! -e refseq_cds_filtered.tsv ]; then
-	./refseq_cds_filter.py --input_file refseq_cds.tsv --output_file refseq_cds_filtered.tsv \
-			--min_seq_length $MIN_SEQ_LEN --trim_seq_length $MIN_SEQ_LEN
+if [ ! -e all_cds.tsv ]; then
+	./refseq_cds_integrate.py --refseq_input_file /work/dacb/detect_hgt/${MIN_SEQ_LEN}bp/refseq_cds_filtered.tsv \
+			--plasmid_input_file plasmids_cds.tsv --min_seq_len 300 \
+			--output_file all_cds.tsv
 fi
 
-if [ ! -e refseq_cds_filtered_balanced.tsv ]; then
-	./refseq_cds_balance.py --tax_level family --taxa Enterobacteriaceae \
-			--output_file refseq_cds_filtered_balanced.tsv \
-			--input_file refseq_cds_filtered.tsv \
+if [ ! -e balanced_cds.tsv ]; then
+	./all_cds_balance.py --input_file all_cds.tsv --output_file balanced_cds.tsv \
 			--positive_samples $CLASS_SAMPLES --random_seed 42
 fi
+
+exit
 
 if [ ! -e refseq_cds_train.mat -o ! -e refseq_cds_valid.mat -o ! -e refseq_cds_test.mat ]; then
 	./refseq_cds_savemat.py --input_file refseq_cds_filtered_balanced.tsv \
