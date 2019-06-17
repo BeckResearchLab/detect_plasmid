049387dbebd86ab45688cf6864e3a0d8ee9e851b
Dave Beck <dacb@uw.edu>
2019-06-17 16:07:20 -0700

    template of fixes for data split

--- prepare.sh
+++ prepare.sh
@@ -37,6 +37,63 @@ if [ ! -e balanced_seq.tsv ]; then
 			--positive_samples $CLASS_SAMPLES --random_seed $RANDOM_SEED
 fi
 
+#### SPLIT OUT THE SETS
+echo fix me
+
+# training
+if [ ! -e training_reads.tsv ]; then
+	echo "generating read like sequences for training data"
+	# this samples sequences like reads, but not necessarily as reads
+	./all_seq_make_reads.py --read_length $MIN_SEQ_LEN 
+			--input_file training_seq.tsv \
+			--output_file training_reads.tsv --random_seed $RANDOM_SEED \
+			--pos_coverage 10 --neg_coverage 1 --class_column 'is_plasmid'
+fi
+
+if [ ! -e training.h5 ]; then
+	echo "encoding and saving training data to hdf5 files"
+	./all_seq_save_hdf5.py --input_file training_reads.tsv \
+			--train_frac 1 --valid_frac 0 --test_frac 0 \
+			--train_file training.h5 --valid_file /tmp/a.h5 \
+			--test_file /tmp/b.h5
+fi
+
+# validation
+if [ ! -e validation_reads.tsv ]; then
+	echo "generating read like sequences for validation data"
+	# this samples sequences like reads, but not necessarily as reads
+	./all_seq_make_reads.py --read_length $MIN_SEQ_LEN 
+			--input_file validation_seq.tsv \
+			--output_file validation_reads.tsv --random_seed $RANDOM_SEED \
+			--pos_coverage 10 --neg_coverage 1 --class_column 'is_plasmid'
+fi
+
+if [ ! -e validation.h5 ]; then
+	echo "encoding and saving validation data to hdf5 files"
+	./all_seq_save_hdf5.py --input_file validation_reads.tsv \
+			--train_frac 0 --valid_frac 1 --test_frac 0 \
+			--train_file /tmp/a.h5 --valid_file /tmp/validation.h5 \
+			--test_file /tmp/b.h5
+fi
+
+# testing
+if [ ! -e testing_reads.tsv ]; then
+	echo "generating read like sequences for testing data"
+	# this samples sequences like reads, but not necessarily as reads
+	./all_seq_make_reads.py --read_length $MIN_SEQ_LEN 
+			--input_file testing_seq.tsv \
+			--output_file testing_reads.tsv --random_seed $RANDOM_SEED \
+			--pos_coverage 10 --neg_coverage 1 --class_column 'is_plasmid'
+fi
+
+if [ ! -e testing.h5 ]; then
+	echo "encoding and saving testing data to hdf5 files"
+	./all_seq_save_hdf5.py --input_file testing_reads.tsv \
+			--train_frac 0 --valid_frac 0 --test_frac 0 \
+			--train_file /tmp/a.h5 --valid_file /tmp/b.h5 \
+			--test_file testing.h5
+fi
+##### 
 if [ ! -e balanced_reads.tsv ]; then
 	echo "generating read like sequences"
 	# this samples sequences like reads, but not necessarily as reads
